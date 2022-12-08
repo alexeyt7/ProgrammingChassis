eafbcc2b5ee95b5e6653c95bbd7df89ff9e580d4
Alexey Tatarinov <alexeyt04@gmail.com>
2022-12-07 20:39:12 -0500

    Don't overwrite CPPFLAGS

--- make_all.bat
+++ make_all.bat
@@ -1,4 +1,4 @@
 touch src/main.cpp
-pros mu -m CPPFLAGS=-Dfar=0 --name 3260_close --slot 1
+pros mu -m CPPFLAGS+=-Dfar=0 --name 3260_close --slot 1
 touch src/main.cpp
-pros mu -m CPPFLAGS=-Dfar=1 --name 3260_far --slot 2
+pros mu -m CPPFLAGS+=-Dfar=1 --name 3260_far --slot 2
